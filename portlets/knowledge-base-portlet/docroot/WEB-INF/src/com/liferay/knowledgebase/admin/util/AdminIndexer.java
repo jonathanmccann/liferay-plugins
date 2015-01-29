5f9288a1f5e7c794792026047bc29aaa23d1bb75
Jonathan McCann <jonathan.mccann@liferay.com>
2015-01-29 08:48:58 -0800

    LPS-53154 Apply LPS-52974 to plugins

--- portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/util/AdminIndexer.java
+++ portlets/knowledge-base-portlet/docroot/WEB-INF/src/com/liferay/knowledgebase/admin/util/AdminIndexer.java
@@ -57,9 +57,7 @@ import javax.portlet.PortletURL;
  */
 public class AdminIndexer extends BaseIndexer {
 
-	public static final String[] CLASS_NAMES = {KBArticle.class.getName()};
-
-	public static final String PORTLET_ID = PortletKeys.KNOWLEDGE_BASE_ADMIN;
+	public static final String CLASS_NAME = KBArticle.class.getName();
 
 	public AdminIndexer() {
 		setDefaultSelectedFieldNames(
@@ -71,13 +69,8 @@ public class AdminIndexer extends BaseIndexer {
 	}
 
 	@Override
-	public String[] getClassNames() {
-		return CLASS_NAMES;
-	}
-
-	@Override
-	public String getPortletId() {
-		return PORTLET_ID;
+	public String getClassName() {
+		return CLASS_NAME;
 	}
 
 	@Override
@@ -129,7 +122,7 @@ public class AdminIndexer extends BaseIndexer {
 	protected Document doGetDocument(Object obj) throws Exception {
 		KBArticle kbArticle = (KBArticle)obj;
 
-		Document document = getBaseModelDocument(PORTLET_ID, kbArticle);
+		Document document = getBaseModelDocument(CLASS_NAME, kbArticle);
 
 		document.addText(
 			Field.CONTENT, HtmlUtil.extractText(kbArticle.getContent()));
@@ -188,11 +181,6 @@ public class AdminIndexer extends BaseIndexer {
 		long companyId = GetterUtil.getLong(ids[0]);
 
 		reindexKBArticles(companyId);
-	}
-
-	@Override
-	protected String getPortletId(SearchContext searchContext) {
-		return PORTLET_ID;
 	}
 
 	protected void reindexKBArticles(KBArticle kbArticle) throws Exception {
